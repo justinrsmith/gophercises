5b2fb83065c993eaf2b649c3faf7a0ac5e16bf29
Justin Smith <justinrsmith88@gmail.com>
2019-05-24 10:17:42 -0500

    part 1 of quiz exercise

--- quiz/quiz.go
+++ quiz/quiz.go
@@ -1,7 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"log"
+	"strings"
+)
 
 func main() {
-	fmt.Printf("hello, world\n")
+	csvFilename := flag.String("csv", "problems.csv", "csv file in format of question, answer")
+	flag.Parse()
+
+	file, err := os.Open(*csvFilename)
+	if err != nil {
+		fmt.Printf("Failed to read file: %s", *csvFilename)
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	problems := parseLines(lines)
+
+	correct := 0
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s = \n", i+1, p.question)
+		var answer string
+		fmt.Scanf("%s\n", &answer)
+		if answer == p.answer {
+			correct++
+		}
+	}
+	fmt.Printf("You scored %d out of %d\n", correct, len(problems))
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			question: line[0],
+			answer: strings.TrimSpace(line[1]),
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	question string
+	answer string
 }
